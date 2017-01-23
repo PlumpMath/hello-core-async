49c26e2924ff2bf8ce8c94158a3560165e7717cb
Žilvinas Urbonas <zilvinas.urbon@gmail.com>
2017-01-23 23:30:08 +0200

    Init simple example

--- pseudo.js
+++ pseudo.js
@@ -1,64 +1,25 @@
 import { dom } from 'goog.dom';
 import { events } from 'goog.events';
-import { XhrIo as xhr } from 'goog.net.Xhr';
-import { go, take, put, Queue} from 'core.async';
+import { go, take, put, chan} from 'core.async';
 
-const YT_SEARCH_URL= "https://www.googleapis.com/youtube/v3/search?key=AIzaSyDqwW1i0Ul8s9yN6nKD5MAhnCCSTzOJDyg&part=snippet&q=";
 
 function listen(el, type) {
-  let out = new Queue();
+  let out = chan();
   events.listen(el, type, (e => {
     put(out, e);
-  }))
+  }));
 
   return out;
 }
 
-function json(uri) {
-  let out = new Queue();
+let clicks = listen(dom.getElement("search"), "click");
 
-  xhr.send(uri, (e) => {
-    var items = e.target.getResponseJson().items;
-    titles = items.map(item => { return item.snippet.title });
-    put(out, titles);
-  })
-
-  return out;
-}
-
-const queryUrl = q => {
-  return `YT_SEARCH_URL${q}`
-};
-
-const userQuery = () => {
-  return dom.getElement("query").value;
-};
-
-const renderQuery = results => {
-  let items = results.map(result => {
-    return `<li>${result}</li>`;
-  }).join("");
-
-  return `<ul>${items}</ul>`;
-}
-
-function init() {
-  let clicks = listen(dom.getElement("search"), "click");
-  let resultsView = dom.getElement("results");
-
-  go(() => {
-    while (true) {
-      take(clicks);
-      let results = take(json(queryUrl(), userQuery()));
-
-      if (results) {
-        resultsView.innerHTML = renderQuery(results);
-      }
-    };
-  });
-}
-
-init();
+go(() => {
+  while (true) {
+    let e = take(clicks);
+    console.log(e);
+  };
+});
 
 
 
--- src/hello_core_async/core.cljs
+++ src/hello_core_async/core.cljs
@@ -2,19 +2,12 @@
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [goog.dom :as dom]
             [goog.events :as events]
-            [goog.net.XhrIo :as xhr]
-            [cljs.core.async :refer [<! put! chan]])
-  (:import [goog Uri]))
+            [cljs.core.async :refer [<! put! chan]]))
 
 ; We will call channels === queues for the ease of explanation
 ;; <!    - take a val from the channel 
 ;; put!  - asynchronously put val into the channel 
 ;; chan  - create a channel 
-;; go    - 
-
-
-(def yt-search-url
- "https://www.googleapis.com/youtube/v3/search?key=AIzaSyDqwW1i0Ul8s9yN6nKD5MAhnCCSTzOJDyg&part=snippet&q=")
 
 (defn listen [el type]
   (let [out (chan)]
@@ -22,35 +15,10 @@
                    (fn [e] (put! out e)))
     out))
 
-(defn json [uri]
-  (let [out (chan)]
-    (xhr/send uri
-      (fn [e]
-        (let [items (-> e .-target .getResponseJson .-items)
-              titles (map #(-> % .-snippet .-title) items)]
-              (put! out titles))))
-    out))
 
-(defn query-url [q]
-  (str yt-search-url q))
+(def clicks (listen (dom/getElement "search") "click"))
 
-(defn user-query []
-  (.-value (dom/getElement "query")))
-
-(defn render-query [results]
-  (str
-    "<ul>"
-    (apply str
-      (for [result results]
-        (str "<li>" result "</li>")))
-    "</ul>"))
-
-(defn init []
-  (let [clicks (listen (dom/getElement "search") "click")
-        results-view (dom/getElement "results")]
-    (go (while true
-          (<! clicks)
-          (let [results (<! (json (query-url (user-query))))]
-            (set! (.-innerHTML results-view) (render-query results)))))))
-
-(init)
+(go
+  (while true
+    (let [e (<! clicks)]
+      (.log js/console e))))
